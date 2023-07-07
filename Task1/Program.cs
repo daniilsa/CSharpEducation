7a4869fd1e1270d5985b1f068805afa18250820c
Daniil Lukashin <lukashin.danilka@mail.ru>
2023-07-07 19:27:39 +0400

    Hello world
    Залил задание вывода строки HelloWorld!

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            Console.ReadLine();
+        }
+    }
+}
