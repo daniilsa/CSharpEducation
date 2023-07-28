403780b008e559a796c0ea3b67f655a60e1076be
Daniil Lukashin <lukashin.danilka@mail.ru>
2023-07-28 18:12:21 +0400

    Коррекция кода
    Вместо static использован singleton

--- Phonebook/Abonent.cs
+++ Phonebook/Abonent.cs
@@ -7,27 +7,39 @@ using System.Threading.Tasks;
 
 namespace Phonebook
 {
-    class Abonent
+  class Abonent
+  {
+    /// <summary>
+    /// Имя пользователя.
+    /// </summary>
+    string name = null;
+
+    /// <summary>
+    /// Телефон пользователя.
+    /// </summary>
+    string phone = null;
+
+    /// <summary>
+    /// Установка или возвращение имя пользователя.
+    /// </summary>
+    public string Name { get { return name; } set { name = value; } }
+
+    /// <summary>
+    /// Установка или возвращение телефона пользователя.
+    /// </summary>
+    public string Phone { get { return phone; } set { phone = value; } }
+
+    public Abonent() : this("None", "None")
     {
-        string name = null;
-        string phone = null;
-
-        public string Name { get {return name; } set {name = value; } }
-        public string Phone { get {return phone; } set { phone = value; } }
-
-        public Abonent()
-        {
-            name = "None";
-            phone = "None";
-        }
-        public Abonent(string phone) : this()
-        {
-            this.name = phone;
-        }
-        public Abonent(string name, string phone) : this()
-        {
-            this.name = name;
-            this.phone = phone;
-        }
     }
+    public Abonent(string phone) : this("None", phone)
+    {
+      this.phone = phone;
+    }
+    public Abonent(string name, string phone)
+    {
+      this.name = name;
+      this.phone = phone;
+    }
+  }
 }
--- Phonebook/ControlPoint.cs
+++ Phonebook/ControlPoint.cs
@@ -6,152 +6,173 @@ using System.Threading.Tasks;
 
 namespace Phonebook
 {
-    public class ControlPoint
+  /// <summary>
+  /// "Панель управления". Предназаечно для выбора и отработки функций программы.
+  /// </summary>
+  public class ControlPoint
+  {
+    private readonly WorkingWithUsersData workingWithUsersData = new WorkingWithUsersData();
+
+    /// <summary>
+    /// Список функций.
+    /// </summary>
+    private void ListActions()
     {
-        /// <summary>
-        /// Список функций
-        /// </summary>
-        private void ListActions()
-        {
-            Console.WriteLine("1. Добавить новую запись");
-            Console.WriteLine("2. Удалить запись");
-            Console.WriteLine("3. Поиск по имени");
-            Console.WriteLine("4. Поиск по телефону");
-            Console.WriteLine("0. Выход");
-        }
-        /// <summary>
-        /// Выбор функции
-        /// </summary>
-        public void CallingActions()
-        {
-            do
-            {
-                Console.Clear();
-                ListActions();
-                Console.Write("Выберите действие:");
-                int i = 0;
-                try
-                {
-                    i = Convert.ToInt32(Console.ReadLine());
-                }
-                catch
-                {
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write("Некорректный ввод данных!");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.WriteLine("Попробуйте снова");
-                    CallingActions();
-                    return;
-                }
-                switch (i)
-                {
-                    case 1: { NewSubscriber(); break; }
-                    case 2: { DeleteSubscriber();  break; }
-                    case 3: { SearchName(); break; }
-                    case 4: { SearchPhone(); break; }
-                    case 0: { return; }
-                    default:
-                        {
-                            Console.Clear();
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write("Некорректный ввод данных!");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.WriteLine("Попробуйте снова");
-                            CallingActions();
-                            return;
-                        }
-                }
-            }
-            while (true);
-        }
-        /// <summary>
-        /// Запись нового пользователя
-        /// </summary>
-        public void NewSubscriber()
-        {
-            Console.Write("Введите имя: ");
-            string name = Console.ReadLine();
-
-            string phone = string.Empty;
-            do
-            {
-                Console.Write("Введите номер: ");
-                phone = Console.ReadLine();
-
-                if (phone != string.Empty)
-                {
-                    if (name != string.Empty)
-                    {
-                        Abonent abonent = new Abonent(name, phone);
-                        WorkingWithData.NewSubscriber(abonent.Name, abonent.Phone);
-                    }
-                    else
-                    {
-                        Abonent abonent = new Abonent(name, phone);
-                        WorkingWithData.NewSubscriber(abonent.Name, abonent.Phone);
-                    }
-                    break;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Поле телефон не может быть пустым!");
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-            }
-            while (true);
-        }
-        /// <summary>
-        /// Удаление пользователя
-        /// </summary>
-        public void DeleteSubscriber()
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("ВНИМАНИЕ! ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Корректно указывайте номер телефона, иначе можете удалить другую запись!");
-            string phone = string.Empty;
-            int index = 0;
-            do
-            {
-                Console.Write("Введите номер: ");
-                phone = Console.ReadLine();
-
-                if (phone != string.Empty)
-                {
-                    WorkingWithData.DeleteSubscriber(phone);
-                    break;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Поле телефон не может быть пустым!");
-                    Console.ForegroundColor = ConsoleColor.White;
-                }
-            }
-            while (true);
-
-            Console.ReadKey();
-        }
-        /// <summary>
-        /// Поиск записи по имени
-        /// </summary>
-        public void SearchName()
-        {
-            Console.Write("Введите имя: ");
-            string name = Console.ReadLine();
-            WorkingWithData.SearchName(name);
-     
-        }
-        /// <summary>
-        /// Поиск записи по номеру телефона
-        /// </summary>
-        public void SearchPhone()
-        {
-            Console.Write("Введите номер телефона: ");
-            string phone = Console.ReadLine();
-            WorkingWithData.SearchPhone(phone);
-        }
+      Console.WriteLine("1. Добавить новую запись");
+      Console.WriteLine("2. Удалить запись");
+      Console.WriteLine("3. Поиск по имени");
+      Console.WriteLine("4. Поиск по телефону");
+      Console.WriteLine("0. Выход");
     }
+    /// <summary>
+    /// Выбор функции.
+    /// </summary>
+    public void CallingActions()
+    {
+     
+      do
+      {
+        Console.Clear();
+        ListActions();
+        Console.Write("Выберите действие:");
+        int i;
+        try
+        {
+          i = Convert.ToInt32(Console.ReadLine());
+        }
+        catch
+        {
+          Console.Clear();
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.Write("Некорректный ввод данных!");
+          Console.ForegroundColor = ConsoleColor.White;
+          Console.WriteLine("Попробуйте снова");
+          CallingActions();
+          return;
+        }
+        switch (i)
+        {
+          case 1:
+            {
+              NewSubscriber();
+              break;
+            }
+          case 2:
+            {
+              DeleteSubscriber();
+              break;
+            }
+          case 3:
+            {
+              SearchName();
+              break;
+            }
+          case 4:
+            {
+              SearchPhone();
+              break;
+            }
+          case 0: return;
+          default:
+            {
+              Console.Clear();
+              Console.ForegroundColor = ConsoleColor.Red;
+              Console.Write("Некорректный ввод данных!");
+              Console.ForegroundColor = ConsoleColor.White;
+              Console.WriteLine("Попробуйте снова");
+              CallingActions();
+              return;
+            }
+        }
+      }
+      while (true);
+    }
+    /// <summary>
+    /// Запись нового пользователя.
+    /// </summary>
+    public void NewSubscriber()
+    {
+      Console.Write("Введите имя: ");
+      string name = Console.ReadLine();
+
+      string phone;
+      do
+      {
+        Console.Write("Введите номер: ");
+        phone = Console.ReadLine();
+
+        if (phone != string.Empty)
+        {
+          if (name != string.Empty)
+          {
+            Abonent abonent = new Abonent(name, phone);
+            workingWithUsersData.CreateNewSubscriber(abonent.Name, abonent.Phone);
+          }
+          else
+          {
+            Abonent abonent = new Abonent(name, phone);
+            workingWithUsersData.CreateNewSubscriber(abonent.Name, abonent.Phone);
+          }
+          break;
+        }
+        else
+        {
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.WriteLine("Поле телефон не может быть пустым!");
+          Console.ForegroundColor = ConsoleColor.White;
+        }
+      }
+      while (true);
+    }
+    /// <summary>
+    /// Удаление пользователя.
+    /// </summary>
+    public void DeleteSubscriber()
+    {
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.Write("ВНИМАНИЕ! ");
+      Console.ForegroundColor = ConsoleColor.White;
+      Console.WriteLine("Корректно указывайте номер телефона, иначе можете удалить другую запись!");
+      string phone;
+      do
+      {
+        Console.Write("Введите номер: ");
+        phone = Console.ReadLine();
+
+        if (phone != string.Empty)
+        {
+          workingWithUsersData.DeleteSubscriber(phone);
+          break;
+        }
+        else
+        {
+          Console.ForegroundColor = ConsoleColor.Red;
+          Console.WriteLine("Поле телефон не может быть пустым!");
+          Console.ForegroundColor = ConsoleColor.White;
+        }
+      }
+      while (true);
+
+      Console.ReadKey();
+    }
+    /// <summary>
+    /// Поиск записи по имени.
+    /// </summary>
+    public void SearchName()
+    {
+      Console.Write("Введите имя: ");
+      string name = Console.ReadLine();
+      workingWithUsersData.SearchName(name);
+
+    }
+    /// <summary>
+    /// Поиск записи по номеру телефона.
+    /// </summary>
+    public void SearchPhone()
+    {
+      Console.Write("Введите номер телефона: ");
+      string phone = Console.ReadLine();
+      workingWithUsersData.SearchPhone(phone);
+    }
+  }
 }
